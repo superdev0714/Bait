d088bc1cd8ab54715a54fa4ff0c55ccfd05b2a8c
superdev <super.dev0714@gmail.com>
2018-07-12 02:34:20 +0200

    fixed an issue;

--- app/src/main/java/com/detect/bait/HomeActivity.java
+++ app/src/main/java/com/detect/bait/HomeActivity.java
@@ -373,23 +373,28 @@ public class HomeActivity extends Activity {
     public void onDestroy()
     {
         Log.e("======", "onDestroy");
-        String userId = firebaseAuth.getCurrentUser().getUid();
+        try {
+            String userId = firebaseAuth.getCurrentUser().getUid();
 
-        final String device_id = Settings.Secure.getString(getApplicationContext().getContentResolver(),
-                Settings.Secure.ANDROID_ID);
+            final String device_id = Settings.Secure.getString(getApplicationContext().getContentResolver(),
+                    Settings.Secure.ANDROID_ID);
 
-        if (mDatabase == null) {
-            FirebaseDatabase database = FirebaseDatabase.getInstance();
-            try {
-                database.setPersistenceEnabled(true);
-            } catch (DatabaseException e) {
-                e.printStackTrace();
+            if (mDatabase == null) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                try {
+                    database.setPersistenceEnabled(true);
+                } catch (DatabaseException e) {
+                    e.printStackTrace();
+                }
+                mDatabase = database.getReference();
             }
-            mDatabase = database.getReference();
-        }
 
-        final DatabaseReference userDatabase = mDatabase.child("users").child(userId);
-        userDatabase.child(device_id).child("isTracking").setValue(false);
+            final DatabaseReference userDatabase = mDatabase.child("users").child(userId);
+            userDatabase.child(device_id).child("isTracking").setValue(false);
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
 
         super.onDestroy();
     }
